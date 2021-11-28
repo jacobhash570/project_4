c24c0e2a6986c1398ada7717d4394fbb34e37778
hinaabidi <83368871+hinaabidi@users.noreply.github.com>
2021-11-27 20:15:05 -0500

    Heroku deployment

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 
 data_for_website = {}
 # link to your database
-engine = create_engine("postgresql://twnoqdzrlkubhw:cfaeb39a268eba169c9b9e2c306ccc7323e76bd4b96e11b9bd1bf3e861bd97d0@ec2-52-2-245-64.compute-1.amazonaws.com:5432/d9quhrtbk5csb8", echo = False)
+engine = create_engine("postgresql://eyjulbqjgoyzac:7dd513f6edde71ea3a743c2483b5ae84a90dccc9b306e098d8c425513a698bba@ec2-52-73-29-239.compute-1.amazonaws.com:5432/d5taik2sir38ga", echo = False)
 #engine = create_engine("postgres://localhost:5432/d9vp4cuu7ce85k", echo = False)
 
 
