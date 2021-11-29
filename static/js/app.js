cb9da519ee78baf39faacfee5b7c500b1fd2c81e
hinaabidi <83368871+hinaabidi@users.noreply.github.com>
2021-11-28 22:37:31 -0500

    Heroku deployment

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,7 @@
 function make_pred(){
+    document.getElementById("calcImage").style.display = "block";
+    document.getElementById("predicted_price").innerHTML = ""
+    document.getElementById("predictLbl").innerHTML = ""
     let carWidth= document.getElementById("car_width").value;
     let carWeight= document.getElementById("car_weight").value;
     let engineSize= document.getElementById("engine_size").value;
@@ -20,15 +23,34 @@ function make_pred(){
 
         } 
     }).then(resp=>{
-        return resp.json()
+        if (resp.status >= 200 && resp.status <= 299) {
+            return resp.json()
+        }
+        else {
+            throw Error(resp.statusText);
+        }
+        
     }).then(resp=>{
-
+        
         console.log(resp)
         var formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
         });
-        document.getElementById("predicted_price").innerHTML = formatter.format(resp.Prediction)
-        document.getElementById("predictLbl").innerHTML = "Predicted car Price is"
-    })
+        setTimeout(function() {
+  		//your code to be executed after 1 second
+  			document.getElementById("calcImage").style.display = "none";
+        	document.getElementById("predicted_price").innerHTML = formatter.format(resp.Prediction)
+        	document.getElementById("predictLbl").innerHTML = "Predicted car Price is"
+		}, delayInMilliseconds);
+        
+        
+    }).catch((error) => {
+    // Handle the error
+        console.log(error);
+        document.getElementById("predictLbl").innerHTML = "Something went wrong. May be because of this - > " + error
+    });
 }
+
+var delayInMilliseconds = 2000; //1 second
+
